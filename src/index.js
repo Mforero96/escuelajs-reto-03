8e10e018760db2dacb3c73e38a58d72c4ea5c133
Mariana Forero <marianaforerotecano@gmail.com>
2019-09-20 14:39:54 -0500

    Reto 3

--- src/index.js
+++ src/index.js
@@ -1,32 +1,70 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+const API = 'https://rickandmortyapi.com/api/character/';
+let xhttp = new XMLHttpRequest();
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
-    }
-  };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
+const fetchData = (url_api) => {
+  return new Promise(function(resolve, reject){
+
+    xhttp.onreadystatechange = () => {
+      try {
+        if (xhttp.readyState === 4) {
+          if (xhttp.status == 200){
+            resolve(JSON.parse(xhttp.responseText) )
+          }
+          else {
+            return reject();
+          }
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    };
+    xhttp.open('GET', url_api, false);//No sea asíncrono
+    xhttp.send();
+  })
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
+const allData = {}; 
+
+fetchData(API)
+  .then(function(data){
+    console.log('Primer Llamado...')
+    allData.data1 = data
+    return fetchData(API + data.results[0].id)
+  })
+  .then(function(data2){
+    console.log('Segundo Llamado...')
+    allData.data2 = data2
+    return fetchData(data2.origin.url)
+  })
+  .then(function(data3){
+    console.log('Tercer Llamado...');
+    allData.data3 = data3
+    console.log(allData.data1.info.count)
+    console.log(allData.data3.dimension)
+  })
+  .catch(
+    function(){
+      console.log('ERROR');
+    }
+  )
+
+/*
+fetchData(API,  (error1, data1) => {
+  if (error1) return console.error(`Error ${error1}`);
   console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+
+  fetchData(`${API} ${data1.results[0].id}`, (error2, data2) => {
     if (error2) return console.error(error1);
     console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
+
+    fetchData(data2.origin.url,  (error3, data3) => {
       if (error3) return console.error(error3);
       console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log(`Personajes ${data1.info.count}`);
+      console.log(`Primer Personaje ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
-});
+});*/
